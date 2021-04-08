2d4434e974e4b1dbe62b1445b90357ad7aacf22d
NiharKashyap <47021184+NiharKashyap@users.noreply.github.com>
2021-04-08 17:18:34 +0530

    Update settings.py

--- diseaseDetection/settings.py
+++ diseaseDetection/settings.py
@@ -151,6 +151,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'leafyai2020@gmail.com'
-EMAIL_HOST_PASSWORD = 'Leafy@20$20'
+EMAIL_HOST_USER = #Add Host username
+EMAIL_HOST_PASSWORD = #Add Host password
 # django_heroku.settings(locals())
